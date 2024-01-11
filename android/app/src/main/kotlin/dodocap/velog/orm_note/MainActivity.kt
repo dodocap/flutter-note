fdb8f834b38ee2dca5d34090640ebd8394687bb3
dodocap <dodocap@naver.com>
2024-01-11 15:52:21 +0900

    initial commit

---
+++ android/app/src/main/kotlin/dodocap/velog/orm_note/MainActivity.kt
@@ -0,0 +1,6 @@
+package dodocap.velog.orm_note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '노트 애플리케이션',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
